f4fd6803f53365ffc618802baa9bb3d46aef6a24
PavanKalyan-18 <pavanklyn18@gmail.com>
2024-09-23 18:22:14 +0530

    DOM commit

---
+++ DOM.js
@@ -0,0 +1,13 @@
+const mainheading = document.getElementById("main-heading");
+  mainheading.textContent = 'Fruit World';
+  mainheading.style.color = 'orange';
+
+  const header = document.getElementById("header");
+  header.style.backgroundColor = "green";
+  header.style.borderBottom = "3px solid orange";
+
+  const basketheading = document.getElementById("basket-heading");
+  basketheading.style.color = 'green';
+
+  const thanks = document.getElementById("thanks");
+  thanks.innerHTML = '<p>Please visit us again</p>';
