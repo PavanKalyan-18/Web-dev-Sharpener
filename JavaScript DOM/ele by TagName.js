d1dc69a5990845a77aeae3e97cb66c9fdb6dafe6
PavanKalyan-18 <pavanklyn18@gmail.com>
2024-09-27 11:50:19 +0530

    made newfolders & Understanding DOM getElementById

--- JavaScript DOM/ele by TagName.js
+++
@@ -1,6 +0,0 @@
-const listItems = document.getElementsByTagName("li");
-listItems[4].style.color = 'blue';
-
-for(let i = 0; i < listItems.length; i++){
-  listItems[i].style.fontStyle = 'italic';
-}
--- JavaScript DOM/ele by name.js
+++
@@ -1,6 +0,0 @@
-const fruit = document.getElementsByClassName("fruit");
-fruit[2].style.backgroundColor = "yellow";
-
-for(let i = 0; i < fruit.length; i++){
-  fruit[i].style.fontWeight = "bold";
-}
